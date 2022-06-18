ee138c8619da40042d9b1419e9ee8938341a2d29
Urich Oliveira <oliveira.urich@gmail.com>
2022-06-18 17:43:10 -0300

    .\Initial commit

---
+++ CSharpAPI/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata;
+using CSharpAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost(Name = "Create new Movie")]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movie.Id = 1;
+        movies.Add(movie);
+    }
+
+    [HttpGet(Name = "Get all Movies")]
+    public IEnumerable<Movie> GetMovies()
+    {
+        return movies;
+    }
+
+    [HttpGet( "{id:int}", Name = "Get Movie")]
+    public IActionResult GetMovie(int id)
+    {
+        foreach (Movie movie in movies)
+        {
+            if (movie.Id == id)
+            {
+                return Ok(movie);
+            }
+        }
+        return NotFound();
+    }
+    // // GET
+    // public IActionResult Index()
+    // {
+    //     return View();
+    // }
+}
---
+++ CSharpAPI/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpAPI.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Rate { get; set; }
+}
